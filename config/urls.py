c3ef4d459964aec98deeb75a1479dcc6cfc1a9a0
Abtinmotiee10 <139640617+Abtinmotiee10@users.noreply.github.com>
2023-07-17 03:47:11 -0700

    dj2

--- config/urls.py
+++ config/urls.py
@@ -15,15 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from .viwes import home, about
+from django.urls import path, include
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",home),
-    path("about",about)
-
+    path("",include("root.urls")),
 
 
 
